09f2ed66b36542ef064864f1f4831b100f3e03aa
Hanae <chacoipi0516@icloud.com>
2020-03-26 21:26:37 +0900

    vscodeへ移行完了

---
+++ コード.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  Logger.log(spreadsheet.getName());//はじめてのGAS
+  
+  var url = 'https://docs.google.com/spreadsheets/d//1n83vW6Ik-seKegml6frSzf8uQMzQ1mxCEBi9XlGtoR4/edit#gid=0'; //URL
+  spreadsheet = SpreadsheetApp.openByUrl(url);
+  Logger.log(spreadsheet.getName()); //はじめてのGAS
+  
+  var id = 'xxxxxxxx'; //スプレッドシートID
+  spreadsheet = SpreadsheetApp.openById(id);
+  Logger.log(spreadsheet.getName());//はじめてのGAS
+}
