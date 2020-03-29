76f6043cd7df5a2198f669e0a14554579d64a6b3
Hanae05 <chacopipi0516@icloud.com>
2020-03-29 23:00:30 +0900

    8-4-2 p224-p225

--- コード.js
+++ コード.js
@@ -1,10 +1,10 @@
 function myFunction() {
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-
-  var sheet = spreadsheet.getSheetByName('シート1');
-  Logger.log(sheet.getName()); //シート1
-
-  var sheets = spreadsheet.getSheets();
-  Logger.log(sheets[0].getName()); //シート1
-  Logger.log(sheets[1].getName()); //シート2
+  var sheet = SpreadsheetApp.getActiveSheet();
+  Logger.log(sheet.getRange('B2').getA1Notation());
+  Logger.log(sheet.getRange('D4:F6').getA1Notation());
+  Logger.log(sheet.getRange('2:2').getA1Notation());
+  Logger.log(sheet.getRange('B:B').getA1Notation());
+  Logger.log(sheet.getRange(4, 4).getA1Notation());
+  Logger.log(sheet.getRange(4, 4, 3).getA1Notation());
+  Logger.log(sheet.getRange(4, 4, 3, 3).getA1Notation());
 }
