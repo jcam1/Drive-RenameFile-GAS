7be16e5c30769b7fd9c7765613be78ba84a8475e
Hanae05 <chacopipi0516@icloud.com>
2020-03-29 21:56:34 +0900

    p211-p218

--- コード.js
+++ コード.js
@@ -1,13 +1,12 @@
 function myFunction() {
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  Logger.log(spreadsheet.getName());//はじめてのGAS
-  
-  var url = 'https://docs.google.com/spreadsheets/d//1n83vW6Ik-seKegml6frSzf8uQMzQ1mxCEBi9XlGtoR4/edit#gid=0'; //URL
+  Logger.log(spreadsheet.getName()); //はじめてのGAS
+
+  var url = 'https://docss.google.com/spreadsheets/d/1YGfE7d4f9aKfNst0W--PDdllA7ArQik-ZhDfqbeXahU/edit#gid=0'; //URL
   spreadsheet = SpreadsheetApp.openByUrl(url);
   Logger.log(spreadsheet.getName()); //はじめてのGAS
-  
-  var id = 'xxxxxxxx'; //スプレッドシートID
-  spreadsheet = SpreadsheetApp.openById(id);
-  Logger.log(spreadsheet.getName());//はじめてのGAS
-}
 
+  var id = '1YGfE7d4f9aKfNst0W--PDdllA7ArQik-ZhDfqbeXahU'; //スプレッドシートID
+  spreadsheet = SpreadsheetApp.openById(id);
+  Logger.log(spreadsheet.getName()); //はじめてのGAS
+}
